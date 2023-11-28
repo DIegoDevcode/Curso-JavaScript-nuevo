f2a5fb877e9fac2cc542fff7e17895dd1b5d56a7
DIegoDevcode <jdiegomoreno05@gmail.com>
2023-11-28 17:04:49 -0500

    Viendo la anatomia de la variables.

---
+++ variables.js
@@ -0,0 +1,13 @@
+let cajaDeAndy = "woody";
+console.log(cajaDeAndy);
+
+//No permitido
+
+let c = "Woody";
+let cd = "Woody";
+let pcAndy = "Woody";
+console.log(cajaDeAndy);
+// Lo permitido
+let primerTrasteoDeAndy = "Woody";
+let UrlDelUsario = "https://www.google.com/?hl=es&safe=active";
+let idDelUsuario = "234567";
