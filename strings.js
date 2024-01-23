b660d2e25f77323f1eb741b2c2303ae493adba43
DIegoDevcode <jdiegomoreno05@gmail.com>
2024-01-23 11:38:08 -0500

    Aprendiendo distintas formas de concatenar strings

--- strings.js
+++ strings.js
@@ -1,30 +1,30 @@
-// Creacion de strings
-const primeraOpcion = "ComillasSimples";
-const segundaOpcion = "Comillas dobles";
+// Creación de strings
+const primeraOpción = "ComillasSimples";
+const segundaOpción = "Comillas dobles";
 
-// Concatenacion de strings: Opcion +
-const direcion = "Calle 40";
+// Concatenación de strings: Opción +
+const dirección = "Calle 40";
 const ciudad = "NewYork";
-const direcionCompleta = "Mi direcion completa es " + direcion + ciudad;
-console.log(direcionCompleta);
+const direcciónCompleta = "Mi dirección completa es " + dirección + ciudad;
+console.log(direcciónCompleta);
 
-const direcionCompletaconespacios = "Mi direccion completa es" + "" + ciudad;
-console.log(direcionCompletaconespacios);
+const direcciónCompletaconespacios = "Mi dirección completa es" + "" + ciudad;
+console.log(direcciónCompletaconespacios);
 
-//2.  Concatenacion de strings: Opcion template literal ''
+//2.  Concatenación de strings: Opción template literal ''
 const nombre = "JuanD";
-const pais = "🇨🇴";
-const presentacion = "Hola, soy ${nombre} de ${pais}";
-console.log(presentacion);
+const país = "🇨🇴";
+const presentación = "Hola, soy ${nombre} de ${país}";
+console.log(presentación);
 
-// 3. Concatenacion de strings: Metodo Join
+// 3. Concatenación de strings: Método Join
 const Parteuno = "Me encanta";
 const Partedos = "la gente de";
 const Partetres = "🇦🇷";
 const Pensamiento = [Parteuno, Partedos, Partetres];
 console.log(Pensamiento.join(""));
 
-// 4. Concatenacion de strings: Metodo concat
+// 4. Concatenación de strings: Método concat
 const Hobbie1 = "Leer Libros";
 const Hobbie2 = "Jugar VideoJuegos";
 const Hobbie3 = "Estudiar";
@@ -37,3 +37,33 @@ const Hobbies = "Mis Hobbies son:".concat(
   ", "
 );
 console.log(Hobbies);
+
+// Caracteres de escape
+// const WhatDoiDO = 'I'm Software Engineer"
+// 1. Escape alternativo
+const escapeAlternativo = "I'm Software Engineer";
+
+// 2. Barra Invertida
+const escapebarraInvertida = "I\' m Software Engineer";
+
+// 3. Template literals
+const escapecomillaInvertida = `I ${'m Software Engineer'} `;
+
+// 4. Escritura de poema
+/*
+De flor en flor va el colibrí,
+De árbol en árbol va carpintero,
+sin importar su destino ambos, 
+cumplen su objetivo. 
+*/
+
+const poema = 'De flor en flor va el colibrí,\n' +
+              'De árbol en árbol va el carpintero,\n' +
+              'sin importar su destino ambos,\n' +
+              'cumplen su objetivo.' 
+console.log(poema)
+const poemainvertido ='De flor en flor va el colibrí,\n\
+De árbol en árbol va el carpintero,\n\
+sin importar su destino ambos,\n\
+cumplen su objetivo.' 
+console.log(poemainvertido)
